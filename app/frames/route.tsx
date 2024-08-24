de8d19202fc7282e8b714dd8c6857d5f37b1e0e7
tieubochet <tao.nguyen712@gmail.com>
2024-08-24 20:27:17 +0700

    v1

---
+++ app/frames/route.tsx
@@ -0,0 +1,189 @@
+import { Button } from "frames.js/next";
+import { frames } from "./frames";
+import { appURL, formatNumber } from "../utils";
+
+interface State {
+  lastFid?: string;
+}
+
+interface MoxieData {
+  today: { allEarningsAmount: string };
+  weekly: { allEarningsAmount: string };
+  lifetime: { allEarningsAmount: string };
+}
+
+const frameHandler = frames(async (ctx) => {
+  interface UserData {
+    name: string;
+    username: string;
+    fid: string;
+    socialCapitalScore: string;
+    socialCapitalRank: string;
+    profileDisplayName: string;
+    isPowerUser: boolean;
+    profileImageUrl: string;
+  }
+
+  let userData: UserData | null = null;
+  let moxieData: MoxieData | null = null;
+
+  let error: string | null = null;
+  let isLoading = false;
+
+  const fetchUserData = async (fid: string) => {
+    isLoading = true;
+    try {
+      const airstackUrl = `${appURL()}/api/farscore?userId=${encodeURIComponent(
+        fid
+      )}`;
+      const airstackResponse = await fetch(airstackUrl);
+      if (!airstackResponse.ok) {
+        throw new Error(
+          `Airstack HTTP error! status: ${airstackResponse.status}`
+        );
+      }
+      const airstackData = await airstackResponse.json();
+
+      if (
+        airstackData.userData.Socials.Social &&
+        airstackData.userData.Socials.Social.length > 0
+      ) {
+        const social = airstackData.userData.Socials.Social[0];
+        userData = {
+          name: social.profileDisplayName || social.profileName || "Unknown",
+          username: social.profileName || "unknown",
+          fid: social.userId || "N/A",
+          profileDisplayName: social.profileDisplayName || "N/A",
+          socialCapitalScore:
+            social.socialCapital?.socialCapitalScore?.toFixed(2) || "N/A",
+          socialCapitalRank: social.socialCapital?.socialCapitalRank || "N/A",
+          isPowerUser: social.isFarcasterPowerUser || false,
+          profileImageUrl:
+            social.profileImageContentValue?.image?.extraSmall ||
+            social.profileImage ||
+            "",
+        };
+      } else {
+        throw new Error("No user data found");
+      }
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      error = (err as Error).message;
+    } finally {
+      isLoading = false;
+    }
+  };
+
+  const fetchMoxieData = async (fid: string) => {
+    try {
+      const moxieUrl = `${appURL()}/api/moxie-earnings?entityId=${encodeURIComponent(
+        fid
+      )}`;
+      const moxieResponse = await fetch(moxieUrl);
+      if (!moxieResponse.ok) {
+        throw new Error(`Moxie HTTP error! status: ${moxieResponse.status}`);
+      }
+      moxieData = await moxieResponse.json();
+    } catch (err) {
+      console.error("Error fetching Moxie data:", err);
+      error = (err as Error).message;
+    }
+  };
+
+  const extractFid = (url: string): string | null => {
+    try {
+      const parsedUrl = new URL(url);
+      let fid = parsedUrl.searchParams.get("userfid");
+
+      console.log("Extracted FID from URL:", fid);
+      return fid;
+    } catch (e) {
+      console.error("Error parsing URL:", e);
+      return null;
+    }
+  };
+
+  let fid: string | null = null;
+
+  if (ctx.message?.requesterFid) {
+    fid = ctx.message.requesterFid.toString();
+    console.log("Using requester FID:", fid);
+  } else if (ctx.url) {
+    fid = extractFid(ctx.url.toString());
+    console.log("Extracted FID from URL:", fid);
+  } else {
+    console.log("No ctx.url available");
+  }
+
+  if (!fid && (ctx.state as State)?.lastFid) {
+    fid = (ctx.state as State).lastFid ?? null;
+    console.log("Using FID from state:", fid);
+  }
+
+  console.log("Final FID used:", fid);
+
+  const shouldFetchData =
+    fid && (!userData || (userData as UserData).fid !== fid);
+
+  if (shouldFetchData && fid) {
+    await Promise.all([fetchUserData(fid), fetchMoxieData(fid)]);
+  }
+
+  const SplashScreen = () => (
+    <img
+      src="https://i.imgur.com/bbMgivK.png"
+      tw="w-full h-full object-cover"
+    />
+  );
+
+  const ScoreScreen = () => {
+    return (
+      <div tw="flex flex-col w-full h-full bg-pink-50 text-blue-800 font-sans">
+        <img
+          src="https://i.imgur.com/uRu621c.png"
+          tw="w-full h-full object-cover"
+        />
+        <span tw="flex absolute bottom-30 left-20 hyphens-manual text-[#EAB142] font-sans text-8xl font-black uppercase subpixel-antialiased ">
+          {userData?.socialCapitalScore} $BNB
+        </span>
+      </div>
+    );
+  };
+  const shareText = encodeURIComponent(
+    userData
+      ? `Claim your BNB rewards`
+      : "Claim your BNB rewards!"
+  );
+
+  // Change the url here
+  const shareUrl = `https://warpcast.com/~/compose?text=${shareText}&embeds[]=https://claim-bnb-rewards.vercel.app/frames${
+    fid ? `?userfid=${fid}` : ""
+  }`;
+
+  const buttons = [];
+
+  if (!userData) {
+    buttons.push(
+      <Button action="post" target={{ href: `${appURL()}?userfid=${fid}` }}>
+        Claim BNB
+      </Button>
+    );
+  } else {
+    buttons.push(
+      <Button action="post" target={{ href: `${appURL()}?userfid=${fid}` }}>
+        Claim BNB
+      </Button>,
+      <Button action="link" target={shareUrl}>
+        Share
+      </Button>
+    );
+  }
+
+  return {
+    image: fid && !error ? <ScoreScreen /> : <SplashScreen />,
+    buttons: buttons,
+  };
+});
+
+export const GET = frameHandler;
+export const POST = frameHandler;
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Claim BNB rewards",
+  description: "use this to claim bnb rewards",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
